c04fd79f7716b530bfc5fdad7103cda6dd3576b2
Isaac Kilbourne <isaac@kilbourne.ca>
2024-06-13 23:31:19 -0400

    No recommends; change shell in GCP-compatible manner
    Tried running this script on a new GCP VM, discovered the chsh call asks for a non-existent password. Changed command to run as root.
    
    Also added --no-install-recommends to package installations

--- launchpad.sh
+++ launchpad.sh
@@ -5,15 +5,15 @@ sudo apt-get update && \
 sudo apt-get upgrade -y
 
 # Install ZSH and Oh My ZSH
-sudo apt-get install zsh -y && \
-chsh -s $(which zsh) && \
+sudo apt-get install zsh -y --no-install-recommends && \
+sudo chsh -s $(which zsh) "$USER" && \
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -- --unattended
 
 # Install Starship.RS
 yes | sh -c "$(curl -sS https://starship.rs/install.sh)"
 
 # Install batcat
-sudo apt-get install bat -y
+sudo apt-get install bat -y --no-install-recommends
 
 # Config setup
 mkdir ~/.config -p && cp ./.config/starship.toml ~/.config/starship.toml
